1f4f4dfc2e14f59350bac88759eb88a806e81ddc
Leonardo Artini <me@leonardoartini.com>
2021-07-18 15:58:08 +0200

    prima commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "ec2-54-220-53-223.eu-west-1.compute.amazonaws.com",
-  USER: "qudlzkumipwypg",
-  PASSWORD: "2e3862566edad9aa323279d14e9b11fe5d637d77e000182e01d98c1e2ffc6b4d",
-  DB: "d4i186br72m8ns",
+  HOST: "hattie.db.elephantsql.com",
+  USER: "qyaapmpe",
+  PASSWORD: "Xpj-CgOqKAF3ycUoQGafxuKEwFCZkev9",
+  DB: "qyaapmpe",
   dialect: "postgres",
   pool: {
     max: 5,
