95cf1d714e5cd23f6c94a0de65d709130af529dc
Leonardo Artini <me@leonardoartini.com>
2021-07-18 12:34:42 +0200

    te pareva 20!

--- app/public/precache-manifest.e5d56c4e907cfabeee3a9032dc5feafb.js
+++ app/public/precache-manifest.e5d56c4e907cfabeee3a9032dc5feafb.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "795569430f4e94833492388528fb9032",
+    "revision": "0de6c146199804d5523dcd014c03f64b",
     "url": "/index.html"
   },
   {
-    "revision": "23b495eb3a86d7a5a819",
+    "revision": "bdc23341662bedbf1943",
     "url": "/static/css/2.63f31f8d.chunk.css"
   },
   {
-    "revision": "7840a88ce662f90553c4",
+    "revision": "e1ad062416b82cf88af6",
     "url": "/static/css/main.52207c00.chunk.css"
   },
   {
-    "revision": "23b495eb3a86d7a5a819",
-    "url": "/static/js/2.9c6a5ae0.chunk.js"
+    "revision": "bdc23341662bedbf1943",
+    "url": "/static/js/2.87036b84.chunk.js"
   },
   {
     "revision": "bfb051adbbec06ed8fb5522cd4b9a1e5",
-    "url": "/static/js/2.9c6a5ae0.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.87036b84.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7840a88ce662f90553c4",
-    "url": "/static/js/main.b537842a.chunk.js"
+    "revision": "e1ad062416b82cf88af6",
+    "url": "/static/js/main.778a8632.chunk.js"
   },
   {
     "revision": "44aee220bccf4ace3598",
--- app/public/service-worker.js
+++ app/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f4ae189c86cfc0d7343eb4d89b56523.js"
+  "/precache-manifest.e5d56c4e907cfabeee3a9032dc5feafb.js"
 );
 
 self.addEventListener('message', (event) => {
