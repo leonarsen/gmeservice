b7ab1454d5e590710d0ef830ae1b097df8ea1c70
Leonardo Artini <me@leonardoartini.com>
2021-07-20 17:00:35 +0200

    1

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "hattie.db.elephantsql.com",
-  USER: "qyaapmpe",
-  PASSWORD: "Xpj-CgOqKAF3ycUoQGafxuKEwFCZkev9",
-  DB: "qyaapmpe",
+  HOST: "ec2-63-33-239-176.eu-west-1.compute.amazonaws.com",
+  USER: "ypwnfgwudvhkpd",
+  PASSWORD: "cc553980ea41d780a5835f789ba1d872a14972b354aa8c3463890e557339aa9a",
+  DB: "d9do7s1mr8014q",
   dialect: "postgres",
   pool: {
     max: 5,
